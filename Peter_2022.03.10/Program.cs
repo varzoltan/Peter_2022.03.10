2dad296f1103e9c8b70ee6b2fece688f2ee5afb4
varzoltan <64203391+varzoltan@users.noreply.github.com>
2022-03-10 17:03:16 +0100

    Add project files.

---
+++ Peter_2022.03.10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Peter_2022._03._10
+{
+    internal class Program
+    {
+        struct Adat
+        {
+                public int szam1, szam2, szam3;
+                public string nev;
+        }
+
+        static void Beolvas()
+        {
+            StreamReader olvas = new StreamReader("TRIATLON.BE");
+            int tombadat = int.Parse(olvas.ReadLine());
+            Adat[] adatok = new Adat[tombadat];
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                adatok[i].nev = olvas.ReadLine();
+                adatok[i].szam1 = int.Parse(olvas.ReadLine());
+                adatok[i].szam2 = int.Parse(olvas.ReadLine());
+                adatok[i].szam3 = int.Parse(olvas.ReadLine());
+            }
+            olvas.Close();
+        }
+        static void Main(string[] args)
+        {
+            Beolvas();
+        }
+    }
+}
